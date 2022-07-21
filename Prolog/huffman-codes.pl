62bbd1a2502c23d677d6add27cfff13ce9a76f3f
Cristian Livella <cristian@cristianlivella.com>
2022-07-21 09:10:33 +0200

    ⚗️  Add some stuff

--- Prolog/huffman-codes.pl
+++ Prolog/huffman-codes.pl
@@ -42,11 +42,119 @@ sort_symbols_and_weights(SymbolsAndWeights, SortedSymbolAndWeights) :-
 create_nodes([[Symbol, Weight]], [[Weight, [Symbol]]]).
 create_nodes([[Symbol, Weight] | Tail], [[Weight, [Symbol]] | NodesTail]) :- create_nodes(Tail, NodesTail).
 
-he_generate_huffman_tree(SymbolsAndWeights, HuffmanTree) :-
-	sort_symbols_and_weights(SymbolsAndWeights, SortedSymbolAndWeights),
-	%create_nodes(SortedSymbolAndWeights, Nodes),
-	HuffmanTree = SortedSymbolAndWeights,
+
+find_node_weight([[Weight, Symbol] | _], Symbol, Weight).
+find_node_weight([_ | Tail], Symbol, Weight) :- find_node_weight(Tail, Symbol, Weight).
+
+% get_nodes_with_sorted_weights(_, _, [], _).
+
+get_nodes_with_sorted_weights(OriginalSymbolsAndWeights, Symbols, [Weight | WeightTail], [[Weight, Symbol] | TailNewSymbolAndWeights]) :-
+	find_node_weight(OriginalSymbolsAndWeights, Symbol, Weight),
+	list_contain(Symbols, Symbol),
+	delete(Symbols, Symbol, NewSymbols),
+	get_nodes_with_sorted_weights(OriginalSymbolsAndWeights, NewSymbols, WeightTail, TailNewSymbolAndWeights),
 	!.
+
+get_nodes_with_sorted_weights(OriginalSymbolsAndWeights, [Symbol], [Weight], [[Weight, Symbol]]) :- !.
+
+extract_weight_node([Symbol, Weight], Symbol, Weight).
+
+extract_weights_nodes([], [], []).
+extract_weights_nodes([Node | Tail], [Weight | TailWeights], [Nodes | TailNodes]) :-
+	extract_weight_node(Node, Weight, Nodes),
+	extract_weights_nodes(Tail, TailWeights, TailNodes).
+
+sort_nodes(Nodes, SortedNodes) :-
+	extract_weights_nodes(Nodes, Weights, SubNodes),
+	sort(0, '@=<', Weights, SortedWeights),
+	get_nodes_with_sorted_weights(Nodes, SubNodes, SortedWeights, SortedNodes).
+
+create_new_node(FirstW, FirstS, SecondW, SecondS, SumWeight, Tail, [[SumWeight, [[FirstW, FirstS], [SecondW, SecondS]]] | Tail]).
+%create_new_node(FirstW, FirstS, SecondW, SecondS, SumWeight, [[SumWeight, [[FirstW, FirstS], [SecondW, SecondS]]]]).
+
+%real_generate_huffman([[FirstW, FirstS], [SecondW, SecondS]], Node) :-
+%	SumWeight is FirstW + SecondW,
+%	create_node_node(FirstW, FirstS, SecondW, SecondS, SumWeight, Node).
+
+real_generate_huffman([[FirstW, FirstS], [SecondW, SecondS] | Rest], Node) :-
+	SumWeight is FirstW + SecondW,
+	create_new_node(FirstW, FirstS, SecondW, SecondS, SumWeight, Rest, Node).
+
+final_generate_huffman([Node], Node).
+
+final_generate_huffman(Nodes, Final) :-
+	sort_nodes(Nodes, SortedNodes),
+	real_generate_huffman(SortedNodes, Partial),
+	final_generate_huffman(Partial, Final).
+
+he_generate_huffman_tree(SymbolsAndWeights, HuffmanTree) :-
+	%sort_symbols_and_weights(SymbolsAndWeights, SortedSymbolAndWeights),
+	create_nodes(SymbolsAndWeights, Nodes),
+	sort_nodes(Nodes, SortedNodes),
+	final_generate_huffman(SortedNodes, HuffmanTree),
+	!.
+
+union([], [], []).
+union([L1 | L1T], L2, [L1 | L3T]) :- union(L1T, L2, L3T), !.
+union([], [L2 | L2T], [L2 | L3T]) :- union([], L2T, L3T).
+
+subnode_generate_symbol_bits([NodeA], Prefix, [[NodeA, Prefix]]) :-
+	atom(NodeA).
+	%union([Prefix], [], NodeABits),
+	%write(Prefix),
+	%write('\n'),
+	%write(NodeABits),
+	%write('\n\n').
+	%string_concat(Prefix, '', NodeABits).
+
+%subnode_generate_symbol_bits([NodeA, NodeB], Prefix, [[[NodeA, NodeABits], [NodeB, NodeBBits]]], _, _) :-
+%	atom(NodeA),
+%	atom(NodeB),
+%	string_concat(Prefix, '', NodeABits),
+%	string_concat(Prefix, '', NodeBBits).
+
+%subnode_generate_symbol_bits([_, [[_, SubNodesA], [_, SubNodesB]]], Prefix, _, _, _) :-
+%	write('A \n').
+
+subnode_generate_symbol_bits([[_, SubNodesA], [_, SubNodesB]], Prefix, Solution) :-
+	%string_concat(Prefix, '0', NodeAPrefix),
+	union(Prefix, ['0'], NodeAPrefix),
+	union(Prefix, ['1'], NodeBPrefix),
+	%string_concat(Prefix, '1', NodeBPrefix),
+	subnode_generate_symbol_bits(SubNodesA, NodeAPrefix, Res1),
+	subnode_generate_symbol_bits(SubNodesB, NodeBPrefix, Res2),
+	union(Res1, Res2, Solution), !.
+				    
+	
+% real_generate_symbol_bits_table([_, SubNodes], Prefix, SymbolBitsTable) :-
+
+get_bits_for_symbol([[FirstSymbol, FirstBits] | TailSymbolBitsTable], FirstSymbol, FirstBits) :- !.
+get_bits_for_symbol([FirstSymbolBitsTable | TailSymbolBitsTable], Symbol, Bits) :- get_bits_for_symbol(TailSymbolBitsTable, Symbol, Bits).
+	
+
+encode_real([], _, []).
+
+encode_real([Symbol | TailMessage], BitsTable, TailBits) :-
+	get_bits_for_symbol(BitsTable, Symbol, Bits),
+	encode_real(TailMessage, BitsTable, TailBits2),
+	write(Bits),
+	write('\n'),
+	write(TailBits2),
+	write('\n'),
+	write(TailBits),
+	write('\n\n'),
+	union(Bits, TailBits2, TailBits).
+
+
+decode_real([Bit | Tail], OtherBits, BitsTable, [Symbol | Message]) :- union( OtherBits, [Bit], Res), get_bits_for_symbol(BitsTable, Symbol, Res), decode_real(Tail, [], BitsTable, Message), !.
+decode_real([Bit | Tail], OtherBits, BitsTable, Message) :- union([Bit], OtherBits, Res), decode_real(Tail, Res, BitsTable, Message), !.
+decode_real(Bits, [], BitsTable, [Symbol | []]) :- get_bits_for_symbol(BitsTable, Symbol, Bits).
+
+
+%he_encode(Message, HuffmanTree, Bits) :-
+	
+
+% he_generate_symbol_bits_table(HuffmanTree, SymbolBitsTable) :-
 	
 
 %%%% end of file -- huffman-codes.pl --
