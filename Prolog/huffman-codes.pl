c34add147c80a85b3e68656d85e0c3d32a3cae31
Cristian Livella <cristian@cristianlivella.com>
2022-07-24 00:02:36 +0200

    🎨 Prolog improvements

--- Prolog/huffman-codes.pl
+++ Prolog/huffman-codes.pl
@@ -5,15 +5,8 @@
 
 %%% UTILS
 
-%%% find_symbol_weight/3
-%%% find_symbol_weight(SymbolsAndWeights, Symbol, Weight)
-%%% Find the SymbolAndWeights couples that match the given Symbol or Weight.
-
-find_symbol_weight([(Symbol, Weight) | _], Symbol, Weight).
-find_symbol_weight([_ | Tail], Symbol, Weight) :- find_symbol_weight(Tail, Symbol, Weight).
-
 %%% list_contain/2
-%%% list_contains(Haystack, Needle) means 'Needle is contained in Haystack')
+%%% list_contains(Haystack, Needle) means 'Needle is contained in Haystack'.
 
 list_contain([ToFind | _], ToFind) :- !.
 list_contain([_ | Tail], ToFind) :- list_contain(Tail, ToFind).
@@ -22,46 +15,45 @@ list_contain([_ | Tail], ToFind) :- list_contain(Tail, ToFind).
 %%% create_nodes(SymbolAndWeights, Nodes)
 %%% Nodes is a list of nodes (leaves) created from SymbolAndWeights.
 
-create_nodes([(Symbol, Weight)], [[Weight, [Symbol]]]).
-create_nodes([(Symbol, Weight) | Tail], [[Weight, [Symbol]] | NodesTail]) :- create_nodes(Tail, NodesTail).
+create_nodes([(Symbol, Weight)], [(Weight, [Symbol])]).
+create_nodes([(Symbol, Weight) | Tail], [(Weight, [Symbol]) | NodesTail]) :- create_nodes(Tail, NodesTail).
 
 %%% find_node_weight/3
-%%% find_node_weight(Nodes, Symbol, Weight)
-%%% The list Nodes contains a node with wheight = Weight and children = Symbol.
+%%% find_node_weight(Nodes, Children, Weight)
+%%% The list Nodes contains a node with weight = Weight and children = Children.
 
-find_node_weight([[Weight, Symbol] | _], Symbol, Weight).
-find_node_weight([_ | Tail], Symbol, Weight) :- find_node_weight(Tail, Symbol, Weight).
+find_node_weight([(Weight, Children) | _], Children, Weight).
+find_node_weight([_ | Tail], Children, Weight) :- find_node_weight(Tail, Children, Weight).
 
 %%% get_nodes_with_sorted_weights/4
-%%% get_nodes_with_sorted_weights(OriginalSymbolsAndWeights, Symbols, Weights, SortedSymbolsAndWeights)
-%%% SortedSymbolsAndWeights is a list that contain the same couples
-%%% contained in OriginalSymbolAndWeights, ordered by weights
-%%% according to Weights list.
+%%% get_nodes_with_sorted_weights(OriginalNodes, ChildrenList, Weights, SortedNodes)
+%%% SortedNodes is a list that contain the same couples
+%%% contained in OriginalNodes, ordered by weights according to Weights list.
 
-get_nodes_with_sorted_weights(OriginalSymbolsAndWeights, Symbols, [Weight | WeightTail], [[Weight, Symbol] | TailNewSymbolAndWeights]) :-
-	find_node_weight(OriginalSymbolsAndWeights, Symbol, Weight),
-	list_contain(Symbols, Symbol),
-	delete(Symbols, Symbol, NewSymbols),
-	get_nodes_with_sorted_weights(OriginalSymbolsAndWeights, NewSymbols, WeightTail, TailNewSymbolAndWeights),
+get_nodes_with_sorted_weights(OriginalNodes, ChildrenList, [Weight | WeightTail], [(Weight, Children) | TailSortedNodes]) :-
+	find_node_weight(OriginalNodes, Children, Weight),
+	list_contain(ChildrenList, Children),
+	delete(ChildrenList, Children, NewChildrenList),
+	get_nodes_with_sorted_weights(OriginalNodes, NewChildrenList, WeightTail, TailSortedNodes),
 	!.
 
-get_nodes_with_sorted_weights(OriginalSymbolsAndWeights, [Symbol], [Weight], [[Weight, Symbol]]) :- !.
+get_nodes_with_sorted_weights(_, [Children], [Weight], [(Weight, Children)]) :- !.
 
 %%% extract_weight_node/3
-%%% extract_weight_node(Node, Weight, Symbol)
-%%% Node has weight = Weight and contains Symbol.
+%%% extract_weight_node(Node, Weight, Children)
+%%% Node has weight = Weight and contains Children.
 
-extract_weight_node([Weight, Symbol], Weight, Symbol).
+extract_weight_node((Weight, Children), Weight, Children).
 
-%%% extract_weights_nodes/3
-%%% extract_weights_nodes(Nodes, Weights, Children)
+%%% extract_weights_children/3
+%%% extract_weights_children(Nodes, Weights, Children)
 %%% The list of nodes SymbolsAndWeights is splitted
 %%% in a list of Weights and a list of Children.
 
-extract_weights_nodes([], [], []).
-extract_weights_nodes([Node | Tail], [Weight | TailWeights], [Nodes | TailNodes]) :-
+extract_weights_children([], [], []).
+extract_weights_children([Node | Tail], [Weight | TailWeights], [Nodes | TailNodes]) :-
 	extract_weight_node(Node, Weight, Nodes),
-	extract_weights_nodes(Tail, TailWeights, TailNodes).
+	extract_weights_children(Tail, TailWeights, TailNodes).
 
 %%% sort_nodes/2
 %%% sort_nodes(Nodes, SortedNodes)
@@ -69,37 +61,36 @@ extract_weights_nodes([Node | Tail], [Weight | TailWeights], [Nodes | TailNodes]
 %%% contained in Nodes, order by weight.
 
 sort_nodes(Nodes, SortedNodes) :-
-	extract_weights_nodes(Nodes, Weights, SubNodes),
+	extract_weights_children(Nodes, Weights, SubNodes),
 	sort(0, '@=<', Weights, SortedWeights),
 	get_nodes_with_sorted_weights(Nodes, SubNodes, SortedWeights, SortedNodes).
 
-%%% prepend_new_node/7
-%%% prepend_new_node(FirstW, FirstC, SecondW, SecondC, SumWeight, Tail, NewNodes).
+%%% prepend_new_node/5
+%%% prepend_new_node(FirstNode, SecondNode, SumWeight, Tail, NewNodes).
 %%% NewNodes contains the nodes in Tail, prepended with a new node
-%%% with weight = SumWeight and with children [FirstW, FirstC] and [SecondW, SecondC].
+%%% with weight = SumWeight and with children FirstNode and SecondNode.
 
-create_new_node(FirstW, FirstS, SecondW, SecondS, SumWeight, Tail, [[SumWeight, [[FirstW, FirstS], [SecondW, SecondS]]] | Tail]).
-%create_new_node(FirstW, FirstS, SecondW, SecondS, SumWeight, [[SumWeight, [[FirstW, FirstS], [SecondW, SecondS]]]]).
+prepend_new_node(FirstNode, SecondNode, SumWeight, Tail, [(SumWeight, [FirstNode, SecondNode]) | Tail]).
 
-%%% real_generate_huffman/3
-%%% real_generate_huffman(Nodes, NewNodes).
+%%% group_first_two_nodes/3
+%%% group_first_two_nodes(Nodes, NewNodes).
 %%% NewNodes contains the same nodes as in Nodes,
 %%% but with the first two nodes moved as children of a new prepended node.
 
-real_generate_huffman([[FirstW, FirstS], [SecondW, SecondS] | Rest], Node) :-
+group_first_two_nodes([(FirstW, FirstC), (SecondW, SecondC) | Rest], Node) :-
 	SumWeight is FirstW + SecondW,
-	create_new_node(FirstW, FirstS, SecondW, SecondS, SumWeight, Rest, Node).
+	prepend_new_node((FirstW, FirstC), (SecondW, SecondC), SumWeight, Rest, Node).
 
-%%% final_generate_huffman/2
-%%% final_generate_huffman(Nodes, Tree)
+%%% generate_huffman_tree/2
+%%% generate_huffman_tree(Nodes, Tree)
 %%% Tree is a huffman tree generated from Nodes.
 
-final_generate_huffman([Node], Node).
+generate_huffman_tree([Node], Node).
 
-final_generate_huffman(Nodes, Final) :-
+generate_huffman_tree(Nodes, Final) :-
 	sort_nodes(Nodes, SortedNodes),
-	real_generate_huffman(SortedNodes, Partial),
-	final_generate_huffman(Partial, Final).
+	group_first_two_nodes(SortedNodes, Partial),
+	generate_huffman_tree(Partial, Final).
 
 %%% union/3
 %%% union(L1, L2, L3)
@@ -111,70 +102,81 @@ union([], [L2 | L2T], [L2 | L3T]) :- union([], L2T, L3T).
 
 %%% generate_symbol_bits_table/3
 %%% generate_symbol_bits_table(Nodes, Prefix, SymbolBitsTable)
+%%% SymbolBitsTable is a list containg couples (Symbol, Bits).
 
 generate_symbol_bits_table([Node], Prefix, [(Node, Prefix)]) :-
 	atom(Node).
 
-generate_symbol_bits_table([[_, SubNodesA], [_, SubNodesB]], Prefix, Solution) :-
+generate_symbol_bits_table([(_, ChildrenA), (_, ChildrenB)], Prefix, Solution) :-
 	union(Prefix, [0], NodeAPrefix),
 	union(Prefix, [1], NodeBPrefix),
-	generate_symbol_bits_table(SubNodesA, NodeAPrefix, Res1),
-	generate_symbol_bits_table(SubNodesB, NodeBPrefix, Res2),
+	generate_symbol_bits_table(ChildrenA, NodeAPrefix, Res1),
+	generate_symbol_bits_table(ChildrenB, NodeBPrefix, Res2),
 	union(Res1, Res2, Solution), !.
 
-% real_generate_symbol_bits_table([_, SubNodes], Prefix, SymbolBitsTable) :-
+%%% get_bits_for_symbol/3
+%%% get_bits_for_symbol(SymbolBitsTable, Symbol, Bits)
+%%% Find the Bits of a given Symbol (or the Symbol of a given Bits list)
+%%% by looking up in the SymbolBitsTable.
 
-get_bits_for_symbol([(FirstSymbol, FirstBits) | TailSymbolBitsTable], FirstSymbol, FirstBits) :- !.
-get_bits_for_symbol([FirstSymbolBitsTable | TailSymbolBitsTable], Symbol, Bits) :- get_bits_for_symbol(TailSymbolBitsTable, Symbol, Bits).
+get_bits_for_symbol([(FirstSymbol, FirstBits) | _], FirstSymbol, FirstBits) :- !.
+get_bits_for_symbol([_ | TailSymbolBitsTable], Symbol, Bits) :- get_bits_for_symbol(TailSymbolBitsTable, Symbol, Bits).
+
+%%% decode/4
+%%% decode(Bits, HeadBits, SymbolBitsTable, Message)
+%%% Message is the decoded Bits, using the provided SymbolBitsTable.
+%%% HeadBits is used internally, it's initially an empty list.
+
+decode([Bit | Tail], OtherBits, SymbolBitsTable, [Symbol | Message]) :-
+	union( OtherBits, [Bit], Res),
+	get_bits_for_symbol(SymbolBitsTable, Symbol, Res),
+	decode(Tail, [], SymbolBitsTable, Message),
+	!.
+
+decode([Bit | Tail], OtherBits, SymbolBitsTable, Message) :-
+	union(OtherBits, [Bit], Res),
+	decode(Tail, Res, SymbolBitsTable, Message),
+	!.
+
+decode(Bits, [], SymbolBitsTable, [Symbol | []]) :-
+	get_bits_for_symbol(SymbolBitsTable, Symbol, Bits).
+
+%%% encode/3
+%%% encode(Message, SymbolBitsTable, Bits)
+%%% Bits is the encoded Message, using the provided SymbolBitsTable.
 
 encode([], _, []).
 
-encode([Symbol | TailMessage], BitsTable, TailBits) :-
-	get_bits_for_symbol(BitsTable, Symbol, Bits),
-	encode(TailMessage, BitsTable, TailBits2),
+encode([Symbol | TailMessage], SymbolBitsTable, TailBits) :-
+	get_bits_for_symbol(SymbolBitsTable, Symbol, Bits),
+	encode(TailMessage, SymbolBitsTable, TailBits2),
 	union(Bits, TailBits2, TailBits).
 
-decode([Bit | Tail], OtherBits, BitsTable, [Symbol | Message]) :-
-	union( OtherBits, [Bit], Res),
-	get_bits_for_symbol(BitsTable, Symbol, Res),
-	decode(Tail, [], BitsTable, Message),
-	!.
-
-decode([Bit | Tail], OtherBits, BitsTable, Message) :-
-	union(OtherBits, [Bit], Res),
-	decode(Tail, Res, BitsTable, Message),
-	!.
-
-decode(Bits, [], BitsTable, [Symbol | []]) :-
-	get_bits_for_symbol(BitsTable, Symbol, Bits).
+%%% read_line/2
+%%% read_line(Stream List)
+%%% List is the list of symbols read in a line from the stream.
 
 read_line(Stream, List) :-
 	read_line_to_codes(Stream, Line),
-	atom_codes(List, Line).
+	atom_codes(String, Line),
+	string_chars(String, List).
 
-read_file(Stream,[]) :-
+%%% read_stream/2
+%%% read_stream(Stream, List)
+%%% List is a list containing the list of symbols of the file lines.
+
+read_stream(Stream, []) :-
     at_end_of_stream(Stream), !.
 
-read_file(Stream,[X|L]) :-
-    \+ at_end_of_stream(Stream),
-    read_line(Stream,X),
-    read_file(Stream,L).
+read_stream(Stream, List) :-
+    read_line(Stream, FirstList),
+    read_stream(Stream, OtherList),
+	union(FirstList, ['\n'], Partial1),
+	union(Partial1, OtherList, List).
 
-lines_to_chars([], []).
-
-lines_to_chars([Line | LineTail], [Chars | CharsTail]) :-
-	string_chars(Line, Chars),
-	lines_to_chars(LineTail, CharsTail).
-
-flat_chars_list([], []).
-flat_chars_list([Line1, Line2 | LineTail], Final) :-
-	union(Line1, ['\n'], Partial1),
-	union(Partial1, Line2, Partial2),
-	union([Partial2], LineTail, Partial4),
-	flat_chars_list(Partial4, Final),
-	!.
-
-flat_chars_list([Line1], Line1).
+%%% write_spaces/1
+%%% write_spaces(Count)
+%%% Write on screen the given number of spaces.
 
 write_spaces(0).
 
@@ -185,31 +187,61 @@ write_spaces(Count) :-
 	write_spaces(NewCount),
 	!.
 
-write_tabs(Count) :-
-	SpaceCount is Count * 4,
-	write_spaces(SpaceCount).
+%%% print_first_half_line/2
+%%% print_first_half_line(IndentLevel, Weight)
+%%% Print the first part of a line of the huffman tree rappresentation.
 
 print_first_half_line(IndentLevel, Weight) :-
-	write_tabs(IndentLevel),
+	Spaces is IndentLevel * 4,
+	write_spaces(Spaces),
 	write('('),
 	write(Weight),
 	write(')').
+
+%%% print_huffman_tree/2
+%%% print_huffman_tree(HuffmanTree, IndentLevel)
+%%% Print the HuffmanTree provided, with the IndentLevel specified.
+
+print_huffman_tree((Weight, [Child1, Child2]), IndentLevel) :-
+	NextIndentLevel is IndentLevel + 1,
+	print_first_half_line(IndentLevel, Weight),
+	write('\n'),
+	print_huffman_tree(Child1, NextIndentLevel),
+	print_huffman_tree(Child2, NextIndentLevel),
+	!.
+
+print_huffman_tree((Weight, [Child]), IndentLevel) :-
+	print_first_half_line(IndentLevel, Weight),
+	write(' '),
+	write(Child),
+	write('\n'),
+	!.
+
+%%% he_decode/3
+%%% he_decode(Bits, HuffmanTree, Message)
+%%% Message is the decoded Bits, using the provided HuffmanTree.
 
 he_decode(Bits, HuffmanTree, Message) :-
 	he_generate_symbol_bits_table(HuffmanTree, SymbolBitsTable),
 	decode(Bits, [], SymbolBitsTable, Message).
 
+%%% he_encode/3
+%%% he_encode(Message, HuffmanTree, Bits)
+%%% Bits is the encoded Message, using the provided HuffmanTree.
+
 he_encode(Message, HuffmanTree, Bits) :-
 	he_generate_symbol_bits_table(HuffmanTree, SymbolBitsTable),
 	encode(Message, SymbolBitsTable, Bits).
 
-he_encode_file(BitTable, Res) :-
-	open('file.txt', read, Str),
-	read_file(Str,Lines),
-	close(Str),
-	lines_to_chars(Lines, Chars),
-	flat_chars_list(Chars, Chars2),
-	encode_real(Chars2, BitTable, Res).
+%%% he_encode_file(File, HuffmanTree, Bits)
+%%% Bits is the encoded content of the file named File,
+%%% using the provided HuffmanTree.
+
+he_encode_file(File, HuffmanTree, Bits) :-
+	open(File, read, Stream),
+	read_stream(Stream, Message),
+	close(Stream),
+	he_encode(Message, HuffmanTree, Bits).
 
 %%% he_generate_huffman_tree/2
 %%% he_generate_huffman_tree(SymbolsAndWeights, HuffmanTree)
@@ -218,25 +250,22 @@ he_encode_file(BitTable, Res) :-
 he_generate_huffman_tree(SymbolsAndWeights, HuffmanTree) :-
 	create_nodes(SymbolsAndWeights, Nodes),
 	sort_nodes(Nodes, SortedNodes),
-	final_generate_huffman(SortedNodes, HuffmanTree),
+	generate_huffman_tree(SortedNodes, HuffmanTree),
 	!.
 
-he_generate_symbol_bits_table([_, Nodes], SymbolBitsTable) :-
+%%% he_generate_symbol_bits_table/2
+%%% he_generate_huffman_tree(HuffmanTree, SymbolBitsTable)
+%%% SymbolBitsTable is a list containg couples (Symbol, Bits),
+%%% generated from the HuffmanTree provided.
+
+he_generate_symbol_bits_table((_, Nodes), SymbolBitsTable) :-
 	generate_symbol_bits_table(Nodes, [], SymbolBitsTable).
 
-he_print_huffman_tree([Weight, [Child1, Child2]], IndentLevel) :-
-	NextIndentLevel is IndentLevel + 1,
-	print_first_half_line(IndentLevel, Weight),
-	write('\n'),
-	he_print_huffman_tree(Child1, NextIndentLevel),
-	he_print_huffman_tree(Child2, NextIndentLevel),
-	!.
+%%% he_print_huffman_tree/1
+%%% he_print_huffman_tree(HuffmanTree)
+%%% Print the given HuffmanTree.
 
-he_print_huffman_tree([Weight, [Child]], IndentLevel) :-
-	print_first_half_line(IndentLevel, Weight),
-	write(' '),
-	write(Child),
-	write('\n'),
-	!.
+he_print_huffman_tree(HuffmanTree) :-
+	print_huffman_tree(HuffmanTree, 0).
 
 %%%% end of file -- huffman-codes.pl --
